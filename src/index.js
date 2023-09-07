0c200bc590ad91fabfd2929471e49b5ead83c4f7
Ehad <ehadberisha@gmail.com>
2023-09-07 03:07:48 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then((Response) => Response.json())
+  .then((data) => renderImgs(data.message));
+
+fetch("https://dog.ceo/api/breeds/list/all")
+  .then((Response) => Response.json())
+  .then((data) => renderBreeds(Object.keys(data.message)));
+
+function renderImgs(urlArr) {
+  console.log(urlArr);
+
+  const divForImgs = document.querySelector("#dog-image-container");
+
+  urlArr.forEach((url) => {
+    const img = document.createElement("img");
+    img.src = url;
+    divForImgs.append(img);
+  });
+}
+
+function renderBreeds(breedArr) {
+  const ulForBreeds = document.querySelector("#dog-breeds");
+  breedArr.forEach((breed) => {
+    const li = document.createElement("li");
+
+    li.textContent = breed;
+
+    li.addEventListener("click", (e) => {
+      e.target.style.color = "pink";
+    });
+
+    ulForBreeds.appendChild(li);
+  });
+}
